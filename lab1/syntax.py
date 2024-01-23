526ffb76e3b78e9a1c79c87eae56fae6dfd839d5
abdikadyr1 <156767411+abdikadyr1@users.noreply.github.com>
2024-01-23 13:48:54 +0600

    Update syntax.py

--- lab1/syntax.py
+++ lab1/syntax.py
@@ -1,8 +1,24 @@
 #1
+>>> print("Hello, World!")
+Hello, World!
+
+#2
+C:\Users\Your Name>python myfile.py
+
+#3 e
+if 5 > 2:
+print("Five is greater than two!")
+
+#4
 if 5 > 2:
   print("Five is greater than two!")
-  
-#2
+
+#5 e
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+#6
 if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
